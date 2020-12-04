be1be9ce4ed186798cd3f97d3eea41f8014bb2c3
Sebastian Amaya <sjamaya@uninorte.edu.co>
2020-12-03 20:28:45 -0500

    back

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const express= require('express');
-const app=express();
-const cors = require('cors');
-//configuraciones
-app.set('port',process.env.PORT || 3000);
-//middlewares
-app.use(express.json());
-app.use(cors());
+const express= require('express')();
+const serverHtpp = require ('http').createServer(express);
+const io= require('socket.io')(serverHtpp);
+
+const Messages=[];
+io.on('connection',(socket)=>{
+    console.log("usuario conectado")
+    socket.on('send-message',(data)=>{
+        Messages.push(data)
+        socket.emit('text-event',Messages);
+        socket.broadcast.emit('text-event',Messages)
+    })
+})
 //Routes
-app.use(require('./src/Routes/estudiantes_Route'));
-app.use(require('./src/Routes/comentarios_Route'));
+//app.use(require('./src/Routes/estudiantes_Route'));
 //Start Server
-app.listen(app.get('port'),()=>{
-    console.log('Servidor corriendo en el puerto',app.get('port'));
+serverHtpp.listen(3000,()=>{
+    console.log("Escuchando en el puerto 3000")
 });
--- src/Config/sequealize.js
+++ src/Config/sequealize.js
@@ -1,14 +1,12 @@
 const Sequelize=require('sequelize');
 //import de los modelos
 const EstudianteModel=require('../Models/estudiantes');
-const CommentsModel=require('../Models/comentarios');
 //configuracion de la url de la bd
 const DBURL ='mysql://root:@localhost:3306/colegio';
 //pasar los parametros de configuracion a sequelize
 const sequelize=new Sequelize(DBURL);
 //creando la tablas tablas
 const Estudiante=EstudianteModel(sequelize,Sequelize);
-const Comment=CommentsModel(sequelize,Sequelize);
 //sincronizando squelize
 sequelize.sync()
     .then(()=>{
@@ -16,6 +14,5 @@ sequelize.sync()
     })
 
 module.exports={
-    Estudiante,
-    Comment
+    Estudiante
 }
